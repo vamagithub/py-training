f86db32433bff11dabf43e508dbc1b8076edb2a5
Vama Mehta <vamagithub@gmail.com>
2023-08-28 11:25:39 +0530

    day - 1

--- arithmetic.py
+++ arithmetic.py
@@ -1,53 +1,53 @@
-a = input("Enter a number: ")
-b = input("Enter another number: ")
-
-print("Choices\n1. for addition\n2. for multiplication\n3. for power\n4. for dividing")
-choice = input("Enter choice: ")
-
-result = False
-
-if choice == 1:
-    result = a + b
-elif choice == 2:
-    result = a * b
-elif choice == 3:
-    result = a ** b
-elif choice == 4:
-    result = a / b
-
-if not result:
-    print("Invalid choice")
-else:
-    print("Result: ", result)
-
-
-
-
-# # ---------------------------
-# # Using match case
-
-# a = int(input("Enter a number: "))
-# b = int(input("Enter another number: "))
+# a = input("Enter a number: ")
+# b = input("Enter another number: ")
 
 # print("Choices\n1. for addition\n2. for multiplication\n3. for power\n4. for dividing")
-# choice = int(input("Enter choice: "))
+# choice = input("Enter choice: ")
 
 # result = False
 
-# match choice:
-#     case 1:
-#         result = a + b
-#     case 2:
-#         result = a * b
-#     case 3:
-#         result = a ** b
-#     case 4:
-#         result = a / b
+# if choice == 1:
+#     result = a + b
+# elif choice == 2:
+#     result = a * b
+# elif choice == 3:
+#     result = a ** b
+# elif choice == 4:
+#     result = a / b
 
 # if not result:
 #     print("Invalid choice")
 # else:
 #     print("Result: ", result)
+
+
+
+
+# ---------------------------
+# Using match case
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+print("Choices\n1. for addition\n2. for multiplication\n3. for power\n4. for dividing")
+choice = int(input("Enter choice: "))
+
+result = False
+
+match choice:
+    case 1:
+        result = a + b
+    case 2:
+        result = a * b
+    case 3:
+        result = a ** b
+    case 4:
+        result = a / b
+
+if not result:
+    print("Invalid choice")
+else:
+    print("Result: ", result)
 
 
 
