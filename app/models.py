9309e18a1fc0f04f4ff667f7ed9d0517f7da5ae8
Vama Mehta <vamagithub@gmail.com>
2023-08-30 11:09:27 +0530

    wip:teacher model

--- app/models.py
+++ app/models.py
@@ -11,3 +11,12 @@ class Student(db.Model):
 
     def __repr__(self):
         return "<User {}>".format(self.name)
+
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), index = True)
+    subject = db.Column(db.String(64))
+
+    def __repr__(self):
+        return f"<Teacher - {self.name}>"
--- teacher.py
+++ teacher.py
@@ -3,3 +3,5 @@ from app import app
 
 if __name__ == '__main__':
     app.run()
+
+    
